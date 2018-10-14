f5b5df507e9a73848f108600203c8bda85ccd522
gnems <g_nemsveridze@cu.edu.ge>
2018-10-14 14:00:12 +0400

    Working on students and subjects

---
+++ Person.js
@@ -0,0 +1,35 @@
+function Person(name, surname, personalNumber){
+    this.name = name;
+    this.surname = surname;
+    this.personalNumber = personalNumber;
+}
+
+function Lecturer(name, surname, personalNumber, subject){
+    Person.call(this, name, surname, personalNumber);
+    this.subject = subject;
+}
+
+function Student(name, surname, personalNumber, studentId){
+    Person.call(this, name, surname, personalNumber);
+    this.studentId = studentId;
+
+    this.subjects = new Array();
+}
+
+Student.prototype.addSubject = function(subjectName, subjectId){
+    this.subjects.push(new Subject(subjectName, subjectId));
+
+};
+
+function Subject(name, subjectId){
+    this.name = name;
+    this.subjectId = subjectId;
+}
+
+var subjects = new Array();
+var students = new Array();
+subjects.push(new Subject("JS", "SW1"));
+students.push(new Student("Guga", "Nemsitsveridze", "1234", "CST123"));
+
+console.log(students);
+console.log(subjects);
