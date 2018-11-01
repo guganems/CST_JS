da9e8ef9b6af00513b72fed93aa3dcdc5faf1707
gnems <g_nemsveridze@cu.edu.ge>
2018-11-01 10:24:10 +0400

    Excer1

---
+++ midtermExces/functions.js
@@ -0,0 +1,7 @@
+function js_style(){
+    let div = document.getElementById('text');
+    div.style.fontFamily = 'monospace';
+    div.style.fontSize = '20px';
+    // სკრიპტის მიბმა არ დაგავიწყდეს
+    div.style.color = 'green';
+}
