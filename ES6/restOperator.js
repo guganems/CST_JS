0cc425a9a4e17937fdd4cc2d3dcb16edec78f3c8
gnems <g_nemsveridze@cu.edu.ge>
2018-11-12 12:48:00 +0400

    sth

---
+++ ES6/restOperator.js
@@ -0,0 +1,7 @@
+function myFun(a, b, ...moreArgs){
+    console.log("a", a);
+    console.log("b", b);
+    console.log("manyMoreArgs", moreArgs);
+}
+
+myFun("one", "two", "three", "four", "five", "six");
